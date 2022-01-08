c136a8e4b2931a5875a0bf7ecf0de703f78954c9
vanshuhassija <vanshuhassija@gmail.com>
2022-01-09 00:49:40 +0530

    Better logging

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ app.listen(PORT, () => {
 // National Stock Exchange (NSE) APIS
 app.get("/", (req, res, next) => {
   res.send("Welcome to Stockwatch API");
-}
+
 });
 // Get the stock market status (open/closed) - JSON
 // Example: http://localhost:3000/get_market_status
 app.get("/get_market_status", (req, res, next) => {
+  console.log("Req is",req);
   NSEAPI.getMarketStatus()
     .then(function (response) {
       res.json(response.data);
+    }).catch(err=>{
+      console.error("Error is",err);
     });
 });
 
